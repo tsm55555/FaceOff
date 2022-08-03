3e67d40475f761a8229a23242e235f9be691f6a1
Anthony Lin <tsm62803@gmail.com>
2022-08-03 07:26:15 +0000

    testing

--- examples/example.py
+++ examples/example.py
@@ -8,8 +8,15 @@ targets = load_data('./faces/target/')
 # Initialize the Attack object with 
 adversarial = Attack(inputs[0], targets[3], optimizer='adam')
 
-# Perform adversarial training
-adversarial_tensor, mask_tensor, img = adversarial.train(detect=True, verbose=True)
+# Perform attack
+adversarial_tensor, mask_tensor, img = adversarial.train(epochs=100, detect=True, verbose=True)
+mask_img = adversarial.view(mask_tensor)
+# print(adversarial_tensor)
+# print(mask_tensor)
 
 # Show the image with mask applied
 img.show()
+img.save("results.jpg")
+mask_img.save("mask.jpg")
+inputs[0].save("input.jpg")
+targets[3].save("target.jpg")
